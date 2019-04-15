15dca5b1f8971e69b401e821859b997e2b0c0e3b
Akbota <akbota.mamadyarova98@gmail.com>
2019-04-15 19:17:24 +0600

    lab 11

---
+++ lab11/todo-front/src/app/shared/models/models.ts
@@ -0,0 +1,12 @@
+export interface ITaskList {
+    id: number;
+    name: string;
+}
+
+export interface ITask {
+    id: number;
+    name: string;
+    created_at: string;
+    due_on: string;
+    status: string;
+}
